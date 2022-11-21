47740d9a66d308ebc5a1c7f7991a4af2b57a4d43
Caroline de Marchi Petherson <104998076+Cahmarchi95@users.noreply.github.com>
2022-11-20 22:24:11 -0300

    update desafio

--- Javascript/teste desafios/validacao-parenteses/main.js
+++ Javascript/teste desafios/validacao-parenteses/main.js
@@ -1,10 +1,10 @@
 //VALIDAÇÃO DE PARÊNTESES
-// Dada uma string com apenas os seguintes caracteres '(', ')', '{', '}', '[', ']' determine se uma determinada string é válida. 
+// Dada uma string com apenas os seguintes caracteres '(', ')', '{', '}', '[', ']' determine se uma determinada string é válida.
 
 // Entrada
 // Uma string é considerada válida se:
 
-// Caracteres de abertura devem ser fechadas pelo mesmo tipo. Abertura devem ser fechados com uma chave correspondente. Uma string vazia é considerada válida. 
+// Caracteres de abertura devem ser fechadas pelo mesmo tipo. Abertura devem ser fechados com uma chave correspondente. Uma string vazia é considerada válida.
 
 // Saída
 // A saída corresponde a um valor Booleano como no exemplo abaixo:
@@ -16,6 +16,18 @@
 // ( )
 
 // true
-// [ ) 
+// [ )
 
 // false
+
+const string = ")";
+
+function isValid(s) {
+  const stack = ["(", ")", "{", "}", "[", "]"];
+  for (let i = 0; i < stack.length; i += 2) {
+    if (s[0] == stack[1] && s[s.length - 1] == stack[i + 1]) return true;
+  }
+  return false;
+}
+
+console.log(isValid(string));
